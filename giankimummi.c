ba40e72617d70d2f248820b8d8f14a9e799f8199
Mohd Ishaan <122714008+Ishaan280804@users.noreply.github.com>
2023-02-20 10:47:51 +0530

    Update giankimummi.c

--- giankimummi.c
+++ giankimummi.c
@@ -1,1 +1,12 @@
-print("nikhil")
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("Enter Number: \n");
+  scanf("%d",&n)
+    for(i=1;i<=10;i++)
+    {
+      printf("%d X %d = %d\n",n,i,n*i)
+    }
+  return 0;
+}
